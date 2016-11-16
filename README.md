0cdfac470bb93a3ecd3c116ed646e648e6a56ef3
Ajay Kumar <soleil.fils.88@gmail.com>
2016-11-16 23:37:34 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## E-Voting Application
+
+- This Application was designed during Final Semester for a Mini Project
+
+# IDE Used
+- Net Beans
---
+++ src/connection/DBase.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connection;
+
+/**
+ *
+ * @author Administrator
+ */
+public class DBase {
+    
+    public static final String userName = "sa";
+    public static final String password = "PASSWORD@123";
+    public static final String url = "jdbc:odbc:MyData";
+      
+
+}
